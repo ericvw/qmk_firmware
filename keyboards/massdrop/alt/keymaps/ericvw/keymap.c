0dcecba7d92bbbe279acd1063ccc52354fdf3fec
Eric N. Vander Weele <ericvw@gmail.com>
2024-10-24 20:03:59 -0400

    alt/ericvw: Add ability to toggle off and on GUI (i.e, Windows) keys
    When gaming, nothing is worse than accidentally hitting the Windows key
    in a critical moment.

--- keyboards/massdrop/alt/keymaps/ericvw/keymap.c
+++ keyboards/massdrop/alt/keymaps/ericvw/keymap.c
@@ -2,6 +2,7 @@
 
 enum LAYERS {
     BASE = 0,
+    GAME,
     MAC,
     ERIC,
     FN,
@@ -14,6 +15,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(FN),  KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [GAME] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______, KC_NO,   _______,                            _______,                            _______, _______, _______, _______, _______
     ),
     [MAC] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -31,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [FN] = LAYOUT_65_ansi_blocker(
-        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE,
-        TO(BASE), _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, _______, KC_END,
-        TG(ERIC), KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU,
-        _______,  RGB_TOG, _______, _______, _______, QK_BOOT, NK_TOGG, DB_TOGG, _______, _______, _______, _______,          KC_PGUP, KC_VOLD,
-        _______,  _______, TG(MAC),                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END
+        KC_GRV,   KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE,
+        TO(BASE), _______,  KC_UP,   _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, _______, KC_END,
+        TG(ERIC), KC_LEFT,  KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU,
+        _______,  RGB_TOG,  _______, _______, _______, QK_BOOT, NK_TOGG, DB_TOGG, _______, _______, _______, _______,          KC_PGUP, KC_VOLD,
+        _______,  TG(GAME), TG(MAC),                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
     /*
     [X] = LAYOUT(
