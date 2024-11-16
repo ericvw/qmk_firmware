b2190f6b2936dc49a1b42d45ef2d821e3025acb2
Eric N. Vander Weele <ericvw@gmail.com>
2024-11-16 12:23:41 -0500

    alt/ericvw: Format code to preferred style
    Essentially ran (n)vim's `=` based on my `cinoptions`.

--- keyboards/massdrop/alt/keymaps/ericvw/keymap.c
+++ keyboards/massdrop/alt/keymaps/ericvw/keymap.c
@@ -28,33 +28,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case RGB_TOG:
-            if (record->event.pressed) {
-              switch (rgb_matrix_get_flags()) {
-                case LED_FLAG_ALL: {
-                    rgb_matrix_set_flags(LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR);
-                    rgb_matrix_set_color_all(0, 0, 0);
-                  }
-                  break;
-                case (LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR): {
-                    rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
-                    rgb_matrix_set_color_all(0, 0, 0);
-                  }
-                  break;
-                case LED_FLAG_UNDERGLOW: {
-                    rgb_matrix_set_flags(LED_FLAG_NONE);
-                    rgb_matrix_disable_noeeprom();
-                  }
-                  break;
-                default: {
-                    rgb_matrix_set_flags(LED_FLAG_ALL);
-                    rgb_matrix_enable_noeeprom();
-                  }
-                  break;
-              }
+      case RGB_TOG:
+        if (record->event.pressed) {
+            switch (rgb_matrix_get_flags()) {
+              case LED_FLAG_ALL: {
+                rgb_matrix_set_flags(LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR);
+                rgb_matrix_set_color_all(0, 0, 0);
+              } break;
+              case (LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER | LED_FLAG_INDICATOR): {
+                rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+                rgb_matrix_set_color_all(0, 0, 0);
+              } break;
+              case LED_FLAG_UNDERGLOW: {
+                rgb_matrix_set_flags(LED_FLAG_NONE);
+                rgb_matrix_disable_noeeprom();
+              } break;
+              default: {
+                rgb_matrix_set_flags(LED_FLAG_ALL);
+                rgb_matrix_enable_noeeprom();
+              } break;
             }
-            return false;
-        default:
-            return true; //Process all other keycodes normally
+        }
+        return false;
+      default:
+        return true; //Process all other keycodes normally
     }
 }
